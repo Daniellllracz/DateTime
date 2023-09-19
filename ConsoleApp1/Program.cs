56154e00c26024f6b83499597b5df7dd3862a9ca
Dániel Rácz <dracz898@gmail.com>
2023-09-19 12:15:31 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //eltel idő
+            TimeSpan ts01 = new TimeSpan(06,30,22);
+            TimeSpan ts02 = new TimeSpan(06,30,22);
+            double td = ts01.TotalDays;
+
+            //datum idő
+            DateTime dt01 = new DateTime(2004, 11,12);
+
+            Console.WriteLine(dt01.ToString("yyyy-MM-dd (dddd)"));
+
+
+            Console.ReadKey(true);
+        } 
+    }
+}
